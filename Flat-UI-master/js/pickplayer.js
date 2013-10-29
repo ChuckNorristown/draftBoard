61b1134f9c820ae4b5289da71a6d4bc6866cc95b
Sean Shields <sean@dmgctrl.com>
2013-10-29 14:45:50 -0400

    working draft logic

---
+++ Flat-UI-master/js/pickplayer.js
@@ -0,0 +1,7 @@
+@{
+    var items = new List<SelectListItem>{
+        new SelectListItem {Value = "1", Text = "QB1"},
+        new SelectListItem {Value = "2", Text = "QB2"},
+        new SelectListItem {Value = "3", Text = "QB3"},
+    };
+}

7b2b1d86a531d93ea8964cb50c5f1e5ac2363023
Sean Shields <sean@dmgctrl.com>
2013-10-31 09:28:09 -0400

    created player database and json loader

---
+++ Flat-UI-master/js/jsonLoader.js
@@ -0,0 +1,3 @@
+$.getJSON("json/player_pool.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+});
--- Flat-UI-master/js/pickplayer.js
+++
@@ -1,7 +0,0 @@
-@{
-    var items = new List<SelectListItem>{
-        new SelectListItem {Value = "1", Text = "QB1"},
-        new SelectListItem {Value = "2", Text = "QB2"},
-        new SelectListItem {Value = "3", Text = "QB3"},
-    };
-}
